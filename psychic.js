6ce7b9c1a68a39953431462b33c3367ed6cb81ac
Bob Kwiencien <bkwiencien@gmail.com>
2017-07-28 13:05:31 -0400

    Jul28-13:05

--- psychic.js
+++ psychic.js
@@ -1,14 +1,22 @@
-var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y,"z"];
-var fitstTime=true;
+var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","t","u","v","w","x","y","z"]
+var firstTime=true;
 var letterIPicked= " ";
+var letterGuessedRaw = " ";
+var letterguessed = " ";
+var letterEntered = " ";
 function yes(){
+  console.log("I am in yes");
   var tempnum = 0
   var gener = 0;
-  if (frstTime) {
-    tempnum = Math.round(Math.random()*100)
+  if (firstTime) {
+    tempnum = Math.round(Math.random()*100);
     gener = tempnum % 25;
-    letterIPicked = alphabet[gener];
+    console.log("gener =" + gener);
+    letterIPicked  = alphabet[gener];
+    console.log(letterIPicked);
+    firstTime = false;
   }
+  letterGuessedRaw=prompt("Guess a letter");
   firstTime=false;
   }
 function assesit() {
