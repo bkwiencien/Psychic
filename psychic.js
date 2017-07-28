2cfb47a1bae8c025546492d09dc4200a160cc3c8
Bob Kwiencien <bkwiencien@gmail.com>
2017-07-28 12:27:04 -0400

    Jul28-12:26

--- psychic.js
+++ psychic.js
@@ -1,3 +1,13 @@
 var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y,"z"];
 var fitstTime=true;
-
+var letterIPicked= " ";
+function yes(){
+  var tempnum = 0
+  var gener = 0;
+  if (frstTime) {
+    tempnum = Math.round(Math.random()*100)
+    gener = tempnum % 25;
+    letterIPicked = alphabet[gener];
+  }
+  firstTime=false;
+  }
