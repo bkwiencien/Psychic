ca6ad4e385073772019a36e1c7cbe728fd99ac68
Bob Kwiencien <bkwiencien@gmail.com>
2017-07-30 12:53:09 -0400

    Jul30-12:52

--- psychic1.js
+++ psychic1.js
@@ -9,43 +9,6 @@ var userText ="";
 var done = false;
 var guessNumber;
 var alreadyGuessed = "";
-function yes(){
-  var tempnum = 0
-  var gener = 0;
-  if (firstTime) {
-    tempnum = Math.round(Math.random()*100);
-    gener = tempnum % 25;
-    letterIPicked  = alphabet[gener];
-    firstTime = false;
-  }
-  while (!done) {
-    guessNumber = numberOfGuesses +1 ;
-    if (numberOfGuesses > 10) {
-     done = true;
-    }
-    letterGuessed = letterGuessedRaw.toLowerCase();
-    console.log("letterGuessed = " + letterGuessed);
-    numberOfGuesses++;
-    var element = document.getElementById("numberofguesses");
-    element.innerHTML = "Number of guesses " + numberOfGuesses;
-    console.log("letterIPicked is " + letterIPicked);
-    console.log("letterGuessed = " + letterGuessed);
-    if (letterGuessed != letterIPicked) {
-       console.log("wrong should change image");
-       var element2 = document.getElementById("mainimage");
-       element2.src= "assets/images/wrong.jpeg";
-       console.log("image should have been changed");
-       console.log("element2 = " + element2);
-       console.log("element2.src = " + element2.src);
-       //alert("Wrong");
-    } else {
-       console.log("correct should change image");
-       var element1 = document.getElementById("mainimage");
-       element1.src="assets/images/correct.jpeg"
-       done = true;
-  }
-}
-}
 function getData() {
    if (firstTime) {
     tempnum = Math.round(Math.random()*100);
